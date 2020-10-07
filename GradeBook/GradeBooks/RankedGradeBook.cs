e8bb5b27dc261b734449befa803ac672f17ec633
JasonVoyna <jason.a.voyna@emcins.com>
2020-10-07 16:20:42 -0500

    Challenge 2.3 Create New GradeBookTypes

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 using GradeBook.Enums;
 using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -17,6 +18,11 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
             }
 
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[threshold - 1])
+                return 'A';
             return 'F';
         }
     }
